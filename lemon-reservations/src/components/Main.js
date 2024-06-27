5ceccd54392f9e9cc92a636233e97d28a40b87fc
Ethan Bossenbroek <ethan.bossenbroek@gmail.com>
2024-06-27 16:04:23 -0400

    change to useReducer hook

--- lemon-reservations/src/components/Main.js
+++ lemon-reservations/src/components/Main.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import HeroLanding from './HeroLanding';
 import Home from './Home';
 import CustomersSay from './CustomersSay';
 import About from './About';
 
 const Main = () => {
-    const [availableTimes, setAvailableTimes] = useState([
-        '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'
-    ]);
+
+    const initializeTimes = () => {
+        return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    };
+
+    const updateTimes = (state, action) => {
+        switch (action.type) {
+            case 'UPDATE_TIMES':
+                return initializeTimes();
+            default:
+                return state;
+        }
+    };
+
+
+    const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
+
 
     return (
         <div>
-            <HeroLanding availableTimes={availableTimes} setAvailableTimes={setAvailableTimes} />
+            <HeroLanding availableTimes={availableTimes} dispatch={dispatch} />
             <Home />
             <CustomersSay />
             <About />
