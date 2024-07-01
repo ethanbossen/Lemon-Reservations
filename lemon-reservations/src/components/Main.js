e3b71b0fd7dd710b4756beb8c7b666e60f7d7be9
Ethan Bossenbroek <ethan.bossenbroek@gmail.com>
2024-07-01 12:16:02 -0400

    Updated to fetch avalible times on date change

--- lemon-reservations/src/components/Main.js
+++ lemon-reservations/src/components/Main.js
@@ -5,13 +5,13 @@ import CustomersSay from './CustomersSay';
 import About from './About';
 
 const initializeTimes = () => {
-    return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    return fetchAPI(new Date());
 };
 
 const updateTimes = (state, action) => {
     switch (action.type) {
         case 'UPDATE_TIMES':
-            return initializeTimes();
+            return fetchAPI(new Date(action.payload));
         default:
             return state;
     }
